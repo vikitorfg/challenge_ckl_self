1bec77df812cf5054ae350703e0423563eedbc24
vikitorfg <victorgarzel@hotmail.com>
2019-10-19 12:01:22 -0300

    add ArticleList component

--- front/client/src/components/ArticlesList.js
+++ front/client/src/components/ArticlesList.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class ArticlesList extends Component {
   render() {
+    console.log(this.props);
+
     return <div>ArticleList</div>;
   }
 }
 
-export default ArticlesList;
+const mapStateToProps = state => {
+  return { props: state };
+};
+
+export default connect(mapStateToProps)(ArticlesList);
--- front/client/src/reducers/index.js
+++ front/client/src/reducers/index.js
@@ -1,23 +1,5 @@
 import { combineReducers } from "redux";
 
-const songsReducer = () => {
-  return [
-    { title: "macarena", duration: "3:05" },
-    { title: "shallow", duration: "2:45" },
-    { title: "no scrubs", duration: "2:34" },
-    { title: "Mambo n 5", duration: "3:46" }
-  ];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === "SELECT_SONG") {
-    return action.payload;
-  }
-
-  return selectedSong;
-};
-
 export default combineReducers({
-  songs: songsReducer,
-  selectedSong: selectedSongReducer
+  replaceMe: () => "sup"
 });
