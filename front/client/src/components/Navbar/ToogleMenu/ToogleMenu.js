ef8e262d1522d5802bcf42921a3b324197c48299
vikitorfg <victorgarzel@hotmail.com>
2019-10-19 16:42:14 -0300

    turn tootlemenu to function

--- front/client/src/components/Navbar/ToogleMenu/ToogleMenu.js
+++ front/client/src/components/Navbar/ToogleMenu/ToogleMenu.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ToogleMenu.css";
 
-export class ToogleMenu extends Component {
-  render() {
-    return (
-      <div>
-        <div className="tooglemenu-items">
-          <ul>
-            {this.props.subjectList.map(subject => {
-              return (
-                <li key={subject}>
-                  <span onClick={() => this.props.handleSubjectChange(subject)}>
-                    {subject}
-                  </span>
-                </li>
-              );
-            })}
-            <li>
-              <div className="tooglemenu-login">LOGIN</div>
-            </li>
-          </ul>
-        </div>
+const ToogleMenu = () => {
+  return (
+    <div>
+      <div className="tooglemenu-items">
+        <ul>
+          {this.props.subjectList.map(subject => {
+            return (
+              <li key={subject}>
+                <span onClick={() => this.props.handleSubjectChange(subject)}>
+                  {subject}
+                </span>
+              </li>
+            );
+          })}
+          <li>
+            <div className="tooglemenu-login">LOGIN</div>
+          </li>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ToogleMenu;
