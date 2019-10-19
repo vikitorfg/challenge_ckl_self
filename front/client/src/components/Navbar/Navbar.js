b17530228986a779b24040d8ce24d027eb876e37
vikitorfg <victorgarzel@hotmail.com>
2019-10-19 16:12:38 -0300

    subject links filter articles

--- front/client/src/components/Navbar/Navbar.js
+++ front/client/src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import logo from "../../static/logo.png";
 import menu from "../../static/menu.png";
 import { connect } from "react-redux";
-// import { fetchArticles } from "../../actions";
+import { fetchArticles } from "../../actions";
 import { fetchSubjects } from "../../actions";
 
 export class Navbar extends Component {
@@ -11,7 +11,6 @@ export class Navbar extends Component {
     this.props.fetchSubjects();
   }
   render() {
-    console.log(this.props.subjects.subjectsReducer);
     return (
       <header className="navbar">
         <nav className="navbar-nav">
@@ -19,10 +18,14 @@ export class Navbar extends Component {
           <img className="navbar-logo" src={logo} alt="logo" />
           <div className="navbar-items">
             <ul>
-              {this.props.subjects.subjectsReducer.map(subject => {
+              {this.props.subjects.map(subject => {
                 return (
                   <li key={subject.name}>
-                    <span>{subject.name}</span>
+                    <span
+                      onClick={() => this.props.fetchArticles(subject.name)}
+                    >
+                      {subject.name}
+                    </span>
                   </li>
                 );
               })}
@@ -40,10 +43,10 @@ export class Navbar extends Component {
 }
 
 const mapStateToProps = state => {
-  return { subjects: state };
+  return { subjects: state.subjectsReducer };
 };
 
 export default connect(
   mapStateToProps,
-  { fetchSubjects }
+  { fetchSubjects, fetchArticles }
 )(Navbar);
