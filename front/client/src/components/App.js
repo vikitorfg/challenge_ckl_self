1613bf22b35d1d323260d804c0c867dda82a0258
vikitorfg <victorgarzel@hotmail.com>
2019-11-13 09:17:25 -0300

    add dinamic color adition to subjects and implement mouse hover color change

--- front/client/src/components/App.js
+++ front/client/src/components/App.js
@@ -6,6 +6,7 @@ import Navbar from "./Navbar/Navbar";
 import ToogleMenu from "./ToogleMenu/ToogleMenu";
 import Login from "./Login/Login";
 import Interests from "./Interests/Interests";
+import Test from "./Test/Test";
 import "./App.css";
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
           <Route path="/subject/:selectedSubject" component={ArticlesList} />
           <Route path="/login" exact component={Login} />
           <Route path="/interests" exact component={Interests} />
+          <Route path="/test" exact component={Test} />
         </div>
       </div>
     </BrowserRouter>
--- front/client/src/components/Interests/Interests.js
+++ front/client/src/components/Interests/Interests.js
@@ -1,25 +1,82 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchSubjects } from "../../actions";
+import Radium, { Style } from "radium";
 import "./interests.css";
 
 class Interests extends Component {
+  convertArrayToObject(array, key) {
+    const initialValue = {};
+    return array.reduce((obj, item) => {
+      return {
+        ...obj,
+        [item[key]]: item
+      };
+    }, initialValue);
+  }
+
+  renderSubjectList() {
+    var style = {};
+    if (this.props.subjects[0]) {
+      style = this.props.subjects.map(subject => {
+        delete subject.id;
+        return {
+          ...subject,
+          ":hover": { backgroundColor: subject.color, color: "white" }
+        };
+      });
+      style = this.convertArrayToObject(style, "name");
+    }
+
+    return this.props.subjects.map(subject => {
+      return (
+        <div
+          key={subject.name}
+          className="choice"
+          style={[style[subject.name]]}
+        >
+          <span>{subject.name}</span>
+        </div>
+      );
+    });
+  }
+
   render() {
-    // inherits much css from Login.css, should refactor that
-    return (
-      <div className="main">
-        <div className="title">
-          WELCOME, <span className="username">USERNAME</span>
+    if (this.props.googleIsSignedIn === false) {
+      return <div className="main">REDIRECT</div>;
+    } else {
+      // inherits much css from Login.css, should refactor that
+      return (
+        <div className="main">
+          <div className="title">
+            WELCOME, <span className="username">USERNAME</span>
+          </div>
+          <div>
+            <div className="subjects">
+              <span className="info-input">MY INTERESTS</span>
+            </div>
+            <div className="subjects-choice">{this.renderSubjectList()}</div>
+          </div>
+          <button className="btn-submit save">SAVE</button>
+          <div className="info-save">
+            <span>BACK TO HOME</span>
+          </div>
         </div>
-        <div className="subjects">
-          <span className="info-input">MY INTERESTS</span>
-        </div>
-        <button className="btn-submit save">SAVE</button>
-        <div className="info-save">
-          <span>BACK TO HOME</span>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
-export default Interests;
+Interests = Radium(Interests);
+
+const mapStateToProps = state => {
+  return {
+    subjects: state.subjectsReducer,
+    googleIsSignedIn: state.googleOauthReducer.isSignedIn
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchSubjects }
+)(Interests);
